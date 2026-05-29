bd99d0b77a39074fc6cf345890439de5b26bb7f1
fabianoreggiani <FABIANOREGGIANI@GMAIL.COM>
2026-05-28 22:42:23 -0300

    Update contact.ts

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -1,6 +1,6 @@
-export const PHONE_SALES = "5541996366543";
+export const PHONE_SALES = "5541996620115";
 export const PHONE_MAINTENANCE = "5541991945563";
-export const PHONE_SALES_DISPLAY = "(41) 99636-6543";
+export const PHONE_SALES_DISPLAY = "(41) 99662-0115";
 export const PHONE_MAINTENANCE_DISPLAY = "(41) 99194-5563";
 
 export const waLink = (phone: string = PHONE_SALES, text: string = "Vim pelo Google do site da Astral Gás e gostaria de um orçamento.") =>
